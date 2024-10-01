d6d38edf6c0f5cf0d1db8391300ea193116c5d1f
larissamato <matoslarissa31@gmail.com>
2024-10-01 12:15:33 -0300

    :sparkles: added packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,83 +1,83 @@
-import { defineConfig } from 'vite'
-import istanbul from 'vite-plugin-istanbul'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import istanbul from "vite-plugin-istanbul";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: "/",
   plugins: [
     react(),
     istanbul({
       cypress: true,
       requireEnv: false,
-      nycrcPath: './.nycrc.json',
-      forceBuildInstrument: true
-    })
+      nycrcPath: "./.nycrc.json",
+      forceBuildInstrument: true,
+    }),
   ],
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
     coverage: {
-      provider: 'instanbul',
-      reporter: ['text', 'html', 'clover']
-    }
+      provider: "instanbul",
+      reporter: ["text", "html", "clover"],
+    },
   },
   optimizeDeps: {
     esbuildOptions: {
-      target: 'esnext'
-    }
+      target: "esnext",
+    },
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
     rollupOptions: {
       output: {
-        chunkFileNames: `[name].[hash].js`
-      }
-    }
+        chunkFileNames: `[name].[hash].js`,
+      },
+    },
   },
   resolve: {
     alias: [
-      { find: '@', replacement: path.resolve(__dirname, './src') },
+      { find: "@", replacement: path.resolve(__dirname, "./src") },
       {
-        find: '@components',
-        replacement: path.resolve(__dirname, './src/components/')
+        find: "@components",
+        replacement: path.resolve(__dirname, "./src/components/"),
       },
       {
-        find: '@common',
-        replacement: path.resolve(__dirname, './src/components/common')
+        find: "@common",
+        replacement: path.resolve(__dirname, "./src/components/common"),
       },
-      { find: '@pages', replacement: path.resolve(__dirname, './src/pages/') },
+      { find: "@pages", replacement: path.resolve(__dirname, "./src/pages/") },
       {
-        find: '@styles',
-        replacement: path.resolve(__dirname, './src/styles/')
-      },
-      {
-        find: '@routes',
-        replacement: path.resolve(__dirname, './src/routes/')
+        find: "@styles",
+        replacement: path.resolve(__dirname, "./src/styles/"),
       },
       {
-        find: '@contexts',
-        replacement: path.resolve(__dirname, './src/contexts/')
+        find: "@routes",
+        replacement: path.resolve(__dirname, "./src/routes/"),
       },
       {
-        find: '@helpers',
-        replacement: path.resolve(__dirname, './src/helpers/')
+        find: "@contexts",
+        replacement: path.resolve(__dirname, "./src/contexts/"),
       },
       {
-        find: '@types',
-        replacement: path.resolve(__dirname, './src/types/')
+        find: "@helpers",
+        replacement: path.resolve(__dirname, "./src/helpers/"),
       },
       {
-        find: '@layouts',
-        replacement: path.resolve(__dirname, './src/layouts/')
+        find: "@types",
+        replacement: path.resolve(__dirname, "./src/types/"),
       },
-      { find: '@hooks', replacement: path.resolve(__dirname, './src/hooks/') },
-      { find: '@tests', replacement: path.resolve(__dirname, './src/tests/') },
       {
-        find: '@constants',
-        replacement: path.resolve(__dirname, './src/constants/')
-      }
-    ]
-  }
-})
+        find: "@layouts",
+        replacement: path.resolve(__dirname, "./src/layouts/"),
+      },
+      { find: "@hooks", replacement: path.resolve(__dirname, "./src/hooks/") },
+      { find: "@tests", replacement: path.resolve(__dirname, "./src/tests/") },
+      {
+        find: "@constants",
+        replacement: path.resolve(__dirname, "./src/constants/"),
+      },
+    ],
+  },
+});
